60c23f2b47dd2576a493289c33dd89e9cbbfc536
sanopsmx <sanjeev@opsmx.io>
2023-05-19 18:09:21 +0530

    Fixed the application creation issue.

--- orca-web/src/main/groovy/com/netflix/spinnaker/orca/Main.groovy
+++ orca-web/src/main/groovy/com/netflix/spinnaker/orca/Main.groovy
@@ -69,7 +69,8 @@ import com.netflix.spinnaker.kork.boot.DefaultPropertiesBuilder
 ])
 @SpringBootApplication(
     scanBasePackages = [
-        "com.netflix.spinnaker.config"
+        "com.netflix.spinnaker.config",
+    "com.netflix.spinnaker.kork"
     ],
     exclude = [BatchAutoConfiguration, GroovyTemplateAutoConfiguration, DataSourceAutoConfiguration]
 )
